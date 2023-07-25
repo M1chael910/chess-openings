0ff609268ea19ee1b6a28f396879de43108e867f
M1chael910 <mpmurphybusiness@gmail.com>
2023-07-25 13:48:32 -0500

    Added header image in the html. Added 2 images and the javascript to change the heading image on click.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-TODO
-// Add code for changing images and descriptions based on a click action
+// This changes the images back and fourth when the user clicks the images
+
+const myImage = document.querySelector("img#heading-logo");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/chess-logo.jpeg") {
+    myImage.setAttribute("src", "images/Chess-img2.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/chess-logo.jpeg");
+    myImage.setAttribute("height", "300");
+    myImage.setAttribute("width", "300");
+  }
+};
