762982028aae8bf62c72524fc41933e2516d1960
M1chael910 <mpmurphybusiness@gmail.com>
2023-08-17 16:05:20 -0500

    Added next move button, centered below image

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // This changes the images back and fourth when the user clicks the images
 
 const myImage = document.querySelector("img#heading-logo");
+const myButton = document.querySelector("button");
 
 myImage.onclick = () => {
+  // This is an anonymous function
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/chess-logo.jpeg") {
     myImage.setAttribute("src", "images/Chess-img2.jpeg");
