7b1da1344f5cdd754614281a9a184ec23e02bba7
M1chael910 <mpmurphybusiness@gmail.com>
2023-09-13 23:28:32 -0500

    Removed font, was causing issues

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 const myImage = document.querySelector("img#heading-logo");
 
-//TODO
 
-const next_move_btn = document.querySelector("next_move_btn");
-const prev_move_btn = document.querySelector("prev_move_btn");
+TODO
+//Fix this shit
+
+const next_move_btn = document.querySelector("#next_move_btn");
+const prev_move_btn = document.querySelector("#prev_move_btn");
 var imageArray = [];
 next_move_btn.onclick = () => {
-const mySrc = myImage.getAttribute("src");
 if (mySrc === "images/QG Basic Opening/QG-1.png") { 
     myImage.setAttribute("src", "images/QG Basic Opening/QG-2.png");
 } else if(mySrc === "images/QG Basic Opening/QG-2.png") { 
